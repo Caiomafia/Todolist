eba443ec5b8733ef1711467ea3bb014cbdb26e7c
Caio Mafra <gamersb200@gmail.com>
2023-01-11 18:03:13 -0300

    Rem0ove

--- README.md
+++
@@ -1,5 +0,0 @@
-# Lista de tarefa 
-<h1>Projeto criado com as tech html,css,javascript  </h1>
-<h3>link:https://caiomafia.github.io/to-do-list2/   </h3>
-![image](https://user-images.githubusercontent.com/91575842/191636814-7ceeacba-0cc5-40a1-b8aa-0ee0d7e2f1d6.png)
-
--- script.js
+++ script.js
@@ -1,64 +1,44 @@
-const inputTArefa = document.querySelector('.input-Tarefas');
-const btnTarefa = document.querySelector('.btn-tarefa');
-const tarefas = document.querySelector('.tarefas');
-const excluirTarefas = document.querySelector('.excluir')
+// Array para armazenar os itens do carrinho
+var cart = [];
 
-
-
-function criarLi(){
-    const li = document.createElement('li');
-   return li;
+// Função para adicionar um item ao carrinho
+function addToCart(product, price) {
+  var item = {
+    product: product,
+    price: price,
+    quantity: 1
+  };
+  
+  // verifica se o item já existe no carrinho
+  var existing = cart.find(function(i) {
+    return i.product === item.product;
+  });
+  
+  if (existing) {
+    existing.quantity++;
+  } else {
+    cart.push(item);
+  }
+  
+  updateCart();
 }
 
-function criarButton(li){  
- const button = document.createElement('button');
-button.classList.add('btnExcluir')
- li.appendChild(button) ;
-  button.innerHTML = '<i class="fa fa-trash"></i>';
+// Função para remover um item do carrinho
+function removeFromCart(product) {
+  cart = cart.filter(function(item) {
+    return item.product !== product;
+  });
+  updateCart();
 }
 
-
-function criarTarefa(textoInput){
-     const li = criarLi();
-    li.innerHTML = textoInput;
-    tarefas.appendChild(li);
-    if(li.classList) li.classList.add("lista");
-    limpaInput();
-    criarButton(li);
-
-}
-
-    
-
-    function limpaInput(){ inputTArefa.value='';  inputTArefa.focus()  ;    }
-    
-    
-inputTArefa.addEventListener('keypress',function(e){
-    if(e.keyCode === 13){criarTarefa(inputTArefa.value)}      } )
-
-
-btnTarefa.addEventListener('click' , function(){ 
-    if(!inputTArefa.value)return; 
-    criarTarefa(inputTArefa.value)  
-             });
-
-           
-
-
-
-
-             document.addEventListener('click' ,function(e){
-                const el = e.target;
-                if(el.classList.contains('btnExcluir')){
-                    el.parentElement.remove();
-                }
-            })
-            
-            document.addEventListener('click' ,function(e){
-                const el = e.target;
-                if(el.classList.contains('excluir')){
-                    tarefas.innerHTML='';
-                }
-            })
-               
-            
+// Função para atualizar a exibição do carrinho
+function updateCart() {
+  var total = 0;
+  var items = cart.map(function(item) {
+    total += item.price * item.quantity;
+    return '<tr>' +
+           '  <td>' + item.product + '</td>' +
+           '  <td>$' + item.price + '</td>' +
+           '  <td>' + item.quantity + '</td>' +
+           '  <td>$' + (item.price * item.quantity) + '</td>' ;
+  }
